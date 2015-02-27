1be96c017c669f4617e769a30983e066f8b52753
Anders Kofoed <anders.kfd@gmail.com>
2015-02-27 12:26:10 +0100

    listenes for data and saves to file

---
+++ hcp-rest-api.py
@@ -0,0 +1,15 @@
+import json
+import bottle
+from bottle import route, run, request, abort
+
+@route('/data', method='PUT')
+def put_data():
+    data = request.body.readline()
+    if not data:
+        abort(400, 'No data received')
+    entity = json.loads(data)
+    f = open('data', 'a')
+    f.write(data + "\n")
+    f.close()
+
+run(host='localhost', port=8080)
